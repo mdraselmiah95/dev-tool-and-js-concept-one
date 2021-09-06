3dd316d6cd3f8a26612ebd7beb32b02375e541de
rasel <russelroy15@gmail.com>
2021-09-06 13:16:48 +0600

    new comment added

--- javaScript-concept.js
+++ javaScript-concept.js
@@ -7,5 +7,29 @@
  6) Scope Chaining 
  7) Closure 
  8) Synchronous & Asynchronous
- 
+
+*/
+
+//Execution context
+
+const name = "programming Hero";
+function programming() {
+  const x = 10;
+  console.log(name);
+  console.log("Codding");
+}
+programming();
+
+//Steps of executing code
+
+/* 
+1. Source code
+2. parser
+3. Abstract window
+4. machine code
+5. execution
+
+context=> 1. Creational phase
+          2. Executional phase
+
 */
