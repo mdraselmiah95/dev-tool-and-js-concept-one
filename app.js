f5b4903d8a7a7d43580fb13964944db9e39872d2
rasel <russelroy15@gmail.com>
2021-09-08 20:58:42 +0600

    new code added

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ let str = "";
 const marks = 60;
 const resubmit = -5;
 // console.log(!(marks > 0 || resubmit > 0));
+
+const person = { name: "hero", id: 101, address: "BD", postalCode: 40321 };
+// console.log(Object.length(person).keys);
+// console.log(Object.keys.length);
+console.log(Object.keys(person).length);
