80536d0d29443ccc710fbce3c8470bd54b43b2cf
rasel <russelroy15@gmail.com>
2021-09-09 18:00:08 +0600

    new code  added

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // second part part
 
-/*
- 1) How JavaScripts Work 
- 2) Execution Context 
- 3) Hoisting 
- 4) Scope
- 5) Lexical Scope 
- 6) Scope Chaining 
- 7) Closure 
- 8) Synchronous & Asynchronous
-*/
-
 // Synchronous
 
 const first = () => console.log("Hi I am first");
