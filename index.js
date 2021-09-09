b9574324c2572d293238da1b41e57d711fb1a805
rasel <russelroy15@gmail.com>
2021-09-09 17:25:20 +0600

    new comment added

---
+++ index.js
@@ -0,0 +1,12 @@
+// second part part
+
+/*
+ 1) How JavaScripts Work 
+ 2) Execution Context 
+ 3) Hoisting 
+ 4) Scope
+ 5) Lexical Scope 
+ 6) Scope Chaining 
+ 7) Closure 
+ 8) Synchronous & Asynchronous
+*/
